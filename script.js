5db1d88af39ad19e7208fc3a1a2ca767f619b83d
sohamjadhao45 <jadhaosoham21@gmail.com>
2026-06-23 16:12:25 +0530

    Add initAudioSpeechEngine to initialization sequence

--- script.js
+++ script.js
@@ -47,7 +47,7 @@
     buildLibrarySystem(); 
     initClockAndAtmosphere(); initUltimateUniverseBackground(); initCosmicNavigation(); 
     initLibraryFeatures(); initScrollProgressBar(); initSecretKeyboardVault(); initLedger();
-    initDynamicShadows(); initBookmarksDrawer(); initFavouritesDrawer(); initTimeCapsule(); initPassport(); initTouchRipple(); init1111Wish();initZenMode();
+    initDynamicShadows(); initBookmarksDrawer(); initFavouritesDrawer(); initTimeCapsule(); initPassport(); initTouchRipple(); init1111Wish();initZenMode();initAudioSpeechEngine();
 
     function initTouchRipple() {
         document.body.addEventListener('click', (e) => {
